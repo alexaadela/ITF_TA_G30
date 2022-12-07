1494193801afcbc0d898ec443e6efbc62003728f
Adela Alexa <adela.alexa@sfappworks.com>
2022-12-07 15:08:34 +0200

    - added curs7

--- prep/c8/id.py
+++ prep/c8/id.py
@@ -1,14 +1,29 @@
+"""
+Elementele dintr-un  site web sunt identificabile prin intermdiul unui tag
+Exemple de tag-uri:
+- id
+- span
+- a (ancora)
+- td (table data)
+- tr (table row)
+- ul (unordered list)
+- ol (ordered list)
+- li (list item)
+- input
+- h (h1,h2,h3,h4,h5,h6)
+- div
+- p (paragraf)
+"""
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-driver.maximize_window()
-
-driver.get('https://signup.heroku.com/')
-time.sleep((2))
-driver.find_element(By.ID, 'onetrust-accept-btn-handler').click()
-driver.find_element(By.ID, "first_name").send_keys(('Test'))
-time.sleep((4))
-driver.quit() # inchide tot browserul si toate tab-urile active
-# driver.close() # inchide doar tab-ul curent
+chrome = webdriver.Chrome()
+time.sleep(3)
+chrome.maximize_window()
+chrome.get("https://formy-project.herokuapp.com/form") # putem deschide un site
+chrome.find_element(By.ID, "first-name").send_keys("Marian")
+chrome.find_element(By.ID, "last-name").send_keys("Gheorghisor")
+time.sleep(3)
+chrome.quit() # inchide toata instanta browserului
+# chrome.close() -> inchide un singur tab (cel activ) din instanta de browser
